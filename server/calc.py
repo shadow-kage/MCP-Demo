cdae7d1ce3f805e46b3dd21a44fbde180f8abc7f
Navaid Wahid <navaid.wahid@nihilent.com>
2025-06-11 15:40:04 +0530

    Updated cacl.py

--- server/calc.py
+++ server/calc.py
@@ -6,9 +6,24 @@ mcp = FastMCP("Demo")
 
 # Add an addition tool
 @mcp.tool()
-def add(a: int, b: int) -> int:
+async def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
+# Add an subtraction tool
+@mcp.tool()
+async def subtract(a: int, b: int) -> int:
+    """Subtract two numbers"""
+    return a - b
+# Add an multiplication tool
+@mcp.tool()
+async def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+# Add an division tool
+@mcp.tool()
+async def divide(a: int, b: int) -> int:
+    """Divide two numbers"""
+    return a / b
 
 
 # Add a dynamic greeting resource
